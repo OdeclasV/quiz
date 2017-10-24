21695b9c9ab3ecf3673fcbdfcbe44496eab29fe7
Victor Salcedo <victorsalc2@gmail.com>
2017-10-23 22:52:39 -0400

    adding new parameter and behavior to play_game; substracting incorrect guesses from total number of guesses; adding a parameter to check_answer(); adding import sys to exit game when max guesses is 0

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+import sys
 
 
 def word_in_blank(word, blanks_in_text):
@@ -7,31 +8,44 @@ def word_in_blank(word, blanks_in_text):
 			return n
 	return None
 
-def check_answer(user_guess,answer_list,word_position):
+def check_answer(user_guess,answer_list,word_position, max_guesses):
 	if user_guess == answer_list[word_position]:
 		print "Correct!\n"
 		return True
-		#return user_guess
 	else:
-		print "Incorrecto!"
-		return False
+		print "Incorrecto!", "You have a total of "+ str(max_guesses - 1) + " guesses"
 
-def play_game(game_string, blanks_in_text, answers):
+# def guesses(guess):
+# 	number = 5
+# 	while number > 0:
+# 		if guess == False:
+# 			number -= 1
+# 			return number 
+# 		else:
+# 			return number
+	# if guess == False:
+	# 	number -= 1
+	# 	return number 
+
+def play_game(game_string, blanks_in_text, answers, max_guesses):
 	replaced = []
-	#answer = False
+	answer = False
 	index = 0
 	game_string = game_string.split()
 	for word in game_string:
 		replacement = word_in_blank(word,blanks_in_text)
 		if replacement != None:
-			user_input = raw_input("Type a " + replacement + " ")
-			answer = check_answer(user_input,answers,index)
-			'''nada mas incluye las respuestas de los blanks cuando esto es correcto'''
-				
+			while not answer and max_guesses != 0:
+				user_input = raw_input("Type a " + replacement + " ")
+				answer = check_answer(user_input,answers,index, max_guesses)
+				if not answer:
+					max_guesses -= 1
+				if max_guesses == 0:
+					sys.exit("Game over")
 			word = word.replace(replacement, user_input)
 			replaced.append(word)
-	
 			index += 1
+			answer = False
 		else:
 			replaced.append(word)
 
@@ -42,14 +56,16 @@ def level_easy():
 	easy_answers = ['Ipsum', 'text', '1500s']
 	blanks_in_text = ["___1___", "___2___", "___3___", "___4___"]
 	easy_para = '''A ___1___ is created with the def keyword. You specify the inputs a ___2___ takes by adding ___3___ separated by commas between the parentheses.'''
+	numb_of_wrong_guesses = 5
 	print "The current paragraph is: \n", easy_para
-	paragraph = play_game(easy_para,blanks_in_text,easy_answers)
+	paragraph = play_game(easy_para,blanks_in_text,easy_answers, numb_of_wrong_guesses)
 
 	print paragraph
 
 level = raw_input("Choose a level: easy, medium, or hard.\n")
 if level == 'easy':
 	print "You chose easy!\n"
+	print "You have a total of 5 guesses"
 	level_easy()
 if level == 'medium':
 	print 'You chose medium!\n'
